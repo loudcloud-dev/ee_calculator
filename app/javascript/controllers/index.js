0c04fe153d8dd177502027a568c49b19b237f07e
Luis Millares <luis@loudcloud.ph>
2024-10-28 14:58:29 +0800

    Add tom-select for multiple select

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,2 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
+import SelectController from "./select_controller";
+
+application.register("select", SelectController);
