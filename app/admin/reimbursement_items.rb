65b7023b3e597bd1ca0364756d9876c86c188ca7
Luis Millares <luis@loudcloud.ph>
2024-10-30 10:06:13 +0800

    Changes in index of reimbursement items

--- app/admin/reimbursement_items.rb
+++ app/admin/reimbursement_items.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register ReimbursementItem do
-  actions :all, except: [:new, :update, :destroy]
+  actions :all, except: [:new, :edit, :update, :destroy]
+
+  index do
+    id_column
+    column :reimbursement_id do |item|
+      link_to item.reimbursement.id, admin_reimbursement_path(item.reimbursement.id)
+    end
+    
+    column :employee_id do |item|
+      link_to item.employee.nickname, admin_employee_path(item.employee.id)
+    end
+
+    column :shared_amount
+
+    actions
+  end
+  
 end
