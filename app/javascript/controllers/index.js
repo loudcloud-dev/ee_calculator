d6d8135d5418cbbd6f0c5d4732f0aa7fe8448d74
Luis Millares <luis@loudcloud.ph>
2024-11-04 20:51:26 +0800

    Changes in show more btn

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 import SelectController from "./select_controller";
+import ToggleController from "./toggle_controller";
 
 application.register("select", SelectController);
+application.register("toggle", ToggleController);
--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import TomSelect from "tom-select";
 
 export default class extends Controller {
   connect() {
