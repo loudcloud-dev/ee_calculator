a9506f55dc17e935b568bfc6f5ed4c41521fdaf9
Luis Millares <luis@loudcloud.ph>
2024-11-04 12:14:31 +0800

    Add month picker and changes in query

--- app/controllers/reimbursements_controller.rb
+++ app/controllers/reimbursements_controller.rb
@@ -5,29 +5,33 @@ class ReimbursementsController < InheritedResources::Base
     @employee = Employee.where(status: 'active').order(:nickname).pluck(:nickname, :id)
     
     if params[:employee_id].present?
-      # Query for filed reimbursements
+      start_date = Date.new(Date.today.year, params[:date][:month].to_i, 5)
+      end_date_month = params[:date][:month].to_i == 12 ? 1 : params[:date][:month].to_i + 1
+      end_date_year = params[:date][:month].to_i == 12 ? Date.today.year + 1 : Date.today.year
+      end_date = Date.new(end_date_year, end_date_month, 4)
+
       @filed_reimbursements = Reimbursement.select('reimbursements.*, categories.name')
                                            .joins(:category)
                                            .where(employee_id: params[:employee_id])
                                            .where.not(status: 'cancelled')
+                                           .order('reimbursements.activity_date DESC')
 
-      # Query for reimbursement items
       @reimbursement_items = ReimbursementItem.select('reimbursements.category_id, SUM(reimbursement_items.shared_amount) AS total_shared_amount')
                                               .joins(:reimbursement)
                                               .where.not(reimbursements: { status: 'cancelled' })
                                               .where(employee_id: params[:employee_id])
-                                              .where('reimbursements.activity_date >= ?', Date.today.at_beginning_of_month + 5.days)
-                                              .where('reimbursements.activity_date <= ?', Date.today.at_beginning_of_month.next_month + 4.days)
+                                              .where('reimbursements.activity_date >= ?', start_date)
+                                              .where('reimbursements.activity_date <= ?', end_date)
                                               .group('reimbursements.category_id')
 
       @item_breakdown = Reimbursement.select('categories.name, reimbursements.activity_date, reimbursements.participated_employee_ids, reimbursement_items.shared_amount')
-                                         .joins(:reimbursement_items)
-                                         .joins(:category)
-                                         .where(reimbursement_items: { employee_id: params[:employee_id] })
-                                         .where('reimbursements.activity_date >= ?', Date.today.at_beginning_of_month + 5.days)
-                                         .where('reimbursements.activity_date <= ?', Date.today.at_beginning_of_month.next_month + 4.days)
-                                         .where.not(status: 'cancelled')
-                                         .order('reimbursements.activity_date ASC')
+                                     .joins(:reimbursement_items)
+                                     .joins(:category)
+                                     .where(reimbursement_items: { employee_id: params[:employee_id] })
+                                     .where('reimbursements.activity_date >= ?', start_date)
+                                     .where('reimbursements.activity_date <= ?', end_date)
+                                     .where.not(status: 'cancelled')
+                                     .order('reimbursements.activity_date DESC')
     end
   end
 
