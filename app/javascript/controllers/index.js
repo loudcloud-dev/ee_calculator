2e6ed80ccd3383eeb79bd2d384bf338fec6e2104
Luis Millares <luis@loudcloud.ph>
2024-11-26 17:57:52 +0800

    Add js validation to invoice image input

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,10 @@ import SelectController from "./select_controller";
 import EmployeeSelectController from "./employee_select_controller";
 import ToggleController from "./toggle_controller";
 import PopoverController from "./popover_controller";
+import ImageValidationController from "./image_validation_controller";
 
 application.register("select", SelectController);
 application.register("employee_select", EmployeeSelectController);
 application.register("toggle", ToggleController);
 application.register("popover", PopoverController);
+application.register("image-validation", ImageValidationController);
