6e6638548b76ba76327fa1822816ee101b9fcb1f
Luis Millares <luis@loudcloud.ph>
2024-11-29 11:20:50 +0800

    Refactor code in exporting image

---
+++ app/services/reimbursement_services/export_reimbursement_images.rb
@@ -0,0 +1,66 @@
+module ReimbursementServices
+  class ExportReimbursementImages < ApplicationService
+    require "zip"
+
+    def initialize(params)
+      @filter_params = params
+    end
+
+    def perform
+      process
+    end
+
+    private
+
+    def process
+      reimbursements = handle_reimbursements_filter(@filter_params)
+      create_temp_zip(reimbursements)
+    end
+
+    def handle_reimbursements_filter(params)
+      reimbursements = Reimbursement.where.not(status: "cancelled").with_attached_image
+
+      if params.present?
+        reimbursements = reimbursements.where(employee_id: params[:employee_id_eq]) if params[:employee_id_eq].present?
+        reimbursements = reimbursements.where(category_id: params[:category_id]) if params[:category_id].present?
+        reimbursements = reimbursements.where(status: params[:status_eq]) if params[:status_eq].present?
+
+        if params[:activity_date_gteq].present? || params[:activity_date_lteq].present?
+          start_date = Date.parse(params[:activity_date_gteq])
+          end_date = params[:activity_date_lteq].present? ? Date.parse(params[:activity_date_lteq]) : Date.today
+
+          reimbursements = reimbursements.where(activity_date: start_date..end_date)
+        end
+      end
+
+      reimbursements
+    end
+
+    def create_temp_zip(reimbursements)
+      temp_dir = Rails.root.join("tmp", "invoice_images")
+
+      FileUtils.rm_rf(temp_dir) if Dir.exist?(temp_dir)
+      FileUtils.mkdir_p(temp_dir)
+
+      zip_file_path = temp_dir.join("images.zip")
+
+      download_zip(zip_file_path, reimbursements)
+      zip_file_path
+    end
+
+    def download_zip(file_path, reimbursements)
+      Zip::File.open(file_path, Zip::File::CREATE) do |zipfile|
+        reimbursements.each_with_index do |reimbursement, index|
+          if reimbursement.image.attached?
+            date = reimbursement.activity_date.strftime("%m%d%Y")
+
+            image_path = ActiveStorage::Blob.service.path_for(reimbursement.image.key)
+            image_filename = "#{date}_#{reimbursement.supplier.upcase}_#{reimbursement.invoice_reference_number}.#{reimbursement.image.filename.extension}"
+
+            zipfile.add(image_filename, image_path)
+          end
+        end
+      end
+    end
+  end
+end
