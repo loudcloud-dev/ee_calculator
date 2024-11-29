91f8d703270b5906bc98db4595c6cd7a27bf12c6
Luis Millares <luis@loudcloud.ph>
2024-11-29 10:33:27 +0800

    Add devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
@@ -43,6 +43,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    address: 'smtp-pulse.com',
+    host: 'smtp-pulse.com',
+    port: '2525',
+    authentication: :login
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
