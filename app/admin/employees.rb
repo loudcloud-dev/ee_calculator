6cbbc5c13a1432e301886dccbeae7e59f824826d
Michelle <michelle@loudcloud.ph>
2024-12-05 12:57:11 +0800

    add devise to employee model

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -3,7 +3,9 @@ ActiveAdmin.register Employee do
                 :last_name,
                 :nickname,
                 :active,
-                :status
+                :status,
+                :employee_type,
+                :email
 
   actions :all, except: [ :destroy ]
 
@@ -16,6 +18,8 @@ ActiveAdmin.register Employee do
     column :first_name
     column :last_name
     column :nickname
+    column :email
+    column :employee_type
     column :active
     column :status do |employee| employee.status.capitalize end
     column :created_at
@@ -30,6 +34,11 @@ ActiveAdmin.register Employee do
       row :last_name
       row :nickname
       row :active
+      row :email
+
+      row :employee_type do |employee|
+        employee.employee_type.capitalize
+      end
 
       row :status do |employee|
         employee.status.capitalize
@@ -46,6 +55,8 @@ ActiveAdmin.register Employee do
       f.input :last_name
       f.input :nickname
       f.input :active
+      f.input :email
+      f.input :employee_type, as: :select, collection: [ [ "Regular", "regular" ], [ "Contractual", "contractual" ], [ "Probationary", "probationary" ] ], include_blank: false
       f.input :status, as: :select, collection: [ [ "Active", "active" ], [ "Inactive", "inactive" ] ], include_blank: false
 
       f.actions
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,16 @@ class Employee < ApplicationRecord
   has_many :reimbursements
   has_many :reimbursement_items
 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   validates :nickname, presence: true, uniqueness: true
+  validates :email, uniqueness: true, allow_nil: true
+
+  protected
+
+  def email_required?
+    # TODO: validate this later on with employee_type
+    false
+  end
 end
