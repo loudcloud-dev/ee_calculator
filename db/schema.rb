78fbfe6e6fb189588e263543c6349c0206f32f60
Michelle <michelle@loudcloud.ph>
2024-12-05 23:31:50 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_03_030155) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_04_064203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -86,6 +86,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_03_030155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "active", default: true, null: false
+    t.string "employee_type"
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.index ["email"], name: "index_employees_on_email", unique: true, where: "(((email)::text <> ''::text) AND (email IS NOT NULL))"
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
   create_table "reimbursement_items", force: :cascade do |t|
