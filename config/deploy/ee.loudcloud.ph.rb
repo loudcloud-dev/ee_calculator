4e6de7ed8ec3f4f078b4e282bf3ab7238c53bcba
Luis Millares <luis@loudcloud.ph>
2024-12-09 11:42:06 +0800

    prod capistrano deploy

--- config/deploy/ee.loudcloud.ph.rb
+++ config/deploy/ee.loudcloud.ph.rb
@@ -1,28 +1,28 @@
 # To load prod db: cap staging.ee.loudcloud.ph db:pull
 
-set :stage, :"staging.ee.loudcloud.ph"
-set :branch, "master"
-set :user, "ee_staging"
+set :stage, :"ee.loudcloud.ph"
+set :branch, "production"
+set :user, "ee_prod"
 
-set :application, "ee_staging"
+set :application, "ee_prod"
 
 # files / directories that are just symlinks to entries in shared
 set :linked_files, %w[ config/database.yml ]
 set :linked_dirs, %w[ log tmp/pids tmp/cache tmp/sockets system public/system storage solr vendor .bundle ]
 
-server "159.65.140.204", user: "ee_staging", roles: %w[web app db]
+server "128.199.78.131", user: "ee_prod", roles: %w[web app db]
 
 set :ssh_options, {
   forward_agent: true
 }
 
 # deploy location
-set :full_app_name, "ee_staging"
-set :deploy_to, "/home/ee_staging/ee_staging"
+set :full_app_name, "ee_prod"
+set :deploy_to, "/home/ee_prod/ee_prod"
 
 # init service name
-set :service, "staging.ee.loudcloud.ph"
-set :worker_service, "staging.ee.loudcloud.ph.sidekiq"
+set :service, "ee.loudcloud.ph"
+set :worker_service, "ee.loudcloud.ph.sidekiq"
 
 # rails settings
 set :rails_env, "production"
