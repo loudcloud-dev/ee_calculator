d37f7630978d0b0943688e62b93c3c8386c18234
Michelle <michelle@loudcloud.ph>
2024-12-14 18:06:36 +0800

    recreate migration in order

--- db/migrate/20241213095121_create_leaves.rb
+++ db/migrate/20241213095121_create_leaves.rb
@@ -3,10 +3,10 @@ class CreateLeaves < ActiveRecord::Migration[8.0]
     create_table :leaves do |t|
       t.references :employee, null: false, foreign_key: true
       t.references :approver, null: false, foreign_key: { to_table: :admin_users }
-      t.date :state_date
+      t.date :start_date
       t.date :end_date
       t.integer :day_count
-      t.string :type
+      t.string :leave_type
       t.string :reason
       t.string :status, default: "pending"
 
