22923102fc969c39becc13b5eacbb8d1f85f4673
Michelle <michelle@loudcloud.ph>
2024-12-15 01:15:52 +0800

    basic form and index

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -1,10 +1,26 @@
 class LeavesController < ApplicationController
+  before_action :authenticate_employee!, only: [ :index, :new, :create ]
   def index
+    @leaves = current_employee.leaves
   end
 
   def new
+    @leave = Leave.new
+    @approvers = AdminUser.order(:email)
   end
 
   def create
+    @leave = current_employee.leaves.build(leave_params)
+    if @leave.save
+      redirect_to leaves_path, notice: "Leave request created successfully!"
+    else
+      render :new, alert: "Failed to create leave request."
+    end
+  end
+
+  private
+
+  def leave_params
+    params.require(:leave).permit(:approver_id, :start_date, :end_date, :day_count, :leave_type, :reason)
   end
 end
