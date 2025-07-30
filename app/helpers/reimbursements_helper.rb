c2c77f3187aa30b41417a87a2309afa62ed511c4
Luis Millares <luis@loudcloud.ph>
2025-07-30 14:31:52 +0800

    Update form design

--- app/helpers/reimbursements_helper.rb
+++ app/helpers/reimbursements_helper.rb
@@ -32,4 +32,22 @@ module ReimbursementsHelper
                 "Good Evening"
     end
   end
+
+  # Returns black or white depending on which contrasts better with the background
+  def text_color_for(hex_color)
+    hex = hex_color.delete('#')
+    r, g, b = hex[0..1].hex, hex[2..3].hex, hex[4..5].hex
+
+    brightness = (r * 299 + g * 587 + b * 114) / 1000
+    brightness > 150 ? '#000000' : '#ffffff'
+  end
+
+  def rgba_color(hex, alpha = 1.0)
+    hex = hex.delete('#')
+    r = hex[0..1].hex
+    g = hex[2..3].hex
+    b = hex[4..5].hex
+
+    "rgba(#{r}, #{g}, #{b}, #{alpha})"
+  end
 end
